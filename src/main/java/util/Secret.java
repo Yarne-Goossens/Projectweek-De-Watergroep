57b99c28836d386896d191300dea301737ecb024
ArnoMerckx <arno.merckx@student.ucll.be>
2022-12-19 10:23:28 +0100

    added credentials to Secret

--- src/main/java/util/Secret.java
+++ src/main/java/util/Secret.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 public class Secret extends Credentials {
 
     public static void setPass(Properties dbProperties) {
-        dbProperties.setProperty("user", "hakkaton_00");
-        dbProperties.setProperty("password", "yourverysecretpasswordhere");
+        dbProperties.setProperty("user", "hakkaton_02");
+        dbProperties.setProperty("password", "Oveechaeto&tfM8o");
     }
 }
