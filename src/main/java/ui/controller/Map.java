1eea60fce03e2bcbb5dcff19d6097817e4b4112b
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2022-12-22 11:16:44 +0100

    map authorization
    Co-authored-by: Yannick <91282328+YannickVandereycken@users.noreply.github.com>
    Co-authored-by: Jan-Willem <72229305+JWsteel@users.noreply.github.com>

--- src/main/java/ui/controller/Map.java
+++ src/main/java/ui/controller/Map.java
@@ -1,5 +1,7 @@
 package ui.controller;
 
+import domain.model.Employee;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +9,10 @@ public class Map  extends RequestHandler {
 
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        Employee user =(Employee) request.getSession().getAttribute("user");
+        if(user == null){
+            throw new NotAuthorizedException();
+        }
         request.setAttribute("services",service.getAllServiceAssignments());
         request.setAttribute("leaks",service.getAllLeaks());
         return "map.jsp";
